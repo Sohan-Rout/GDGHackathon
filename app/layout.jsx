46ba9fa3c9a149a13ab0b8ad92118e191b8b7b18
Sohan Rout <sohanrout84@gmail.com>
2025-08-23 21:43:31 +0530

    Style : added og image

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,23 @@ import "./globals.css";
 export const metadata = {
   title: "Hack'O'Relay 4.0",
   description: "A 36 hour hacathon oragnized by gdg gurugram",
+
+  openGraph: {
+    title: 'Hack\'O\'relay-4.0',
+    description: 'Code. Create. Conquer. 36-Hour Innovation Challenge. A Hackathon for the Next Generation of Developers.',
+    url: 'gdg-hackathon-five.vercel.app',
+    siteName: 'HackORelay',
+    images: [
+      {
+        url: '/og.jpeg',
+        width: 1200,
+        height: 630,
+        alt: 'Preview Image',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
